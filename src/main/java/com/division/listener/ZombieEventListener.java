d705e97b46fb4b2e7e02bcf6510a46f51cacd654
LemonCaramel <lemoncaramel_@naver.com>
2021-05-02 14:35:13 +0900

    주식을 가지고 있지 않을 때 발생하는 NPE 오류

--- src/main/java/com/division/listener/ZombieEventListener.java
+++ src/main/java/com/division/listener/ZombieEventListener.java
@@ -2,6 +2,7 @@ package com.division.listener;
 
 import com.division.data.DataManager;
 import com.division.data.StockManager;
+import com.division.data.UserStock;
 import moe.caramel.counterzombie.api.caramel.event.ZombieSettingChangeKickEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -18,7 +19,8 @@ public class ZombieEventListener implements Listener {
     @EventHandler
     public void onZombieSettingChangeKick(ZombieSettingChangeKickEvent event) {
         Player target = event.getPlayer();
-        StockManager.getInstance().getUserStockData().get(target.getUniqueId()).clear();
+        UserStock stock = StockManager.getInstance().getUserStockData().get(target.getUniqueId());
+        if (stock != null) stock.clear();
         target.sendMessage(header + "§c게임 탈주 패널티로 모든 주식이 삭제되었습니다.");
     }
 }
